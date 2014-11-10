bb30c11ea9585f106431a37d68a2d6940b077a6c
Andrea Giardini <contact@andreagiardini.com>
2014-11-10 14:39:04 +0100

    Small clean

--- buildScript.sh
+++ buildScript.sh
@@ -2,24 +2,11 @@
 
 pushd ~
 
-git clone https://github.com/pulp/pulp
-pushd pulp
-patch -p1 < ../api.patch
-git add .
-git commit -am "Apply patch"
-#git checkout 2.5-dev
-tito build --rpm --test
-pushd agent
-tito build --rpm --test
-popd
-popd
-
-for r in pulp_puppet pulp_rpm pulp_ostree nectar pulp_openstack; do
-    echo "checking out $r code"
+for r in pulp pulp_puppet pulp_rpm pulp_ostree nectar pulp_openstack; do
+    echo "Cloning $r repo..."
     git clone https://github.com/pulp/$r
-    echo "installing $r dev code"
+    echo "Creating packages..."
     pushd $r
-    #git checkout 2.5-dev
     tito build --rpm --test
     popd
 done
