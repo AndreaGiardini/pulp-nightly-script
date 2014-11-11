c2d29513aa1ca4e1aa3e1947b679adb7a655193d
Andrea Giardini <contact@andreagiardini.com>
2014-11-11 16:31:31 +0100

    Added Vagrant and PR build

--- buildScript.sh
+++ buildScript.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
+#SETTINGS
+projects='pulp pulp_puppet pulp_rpm pulp_ostree nectar pulp_openstack'
+repoFolder='/var/www/html/pulp/'
+
+pullReqs="$(curl --silent https://github.com/pulp/pulp/pulls | grep "^        #" | cut -c 10-)"
+
+mkdir -p "$repoFolder"
+
 pushd ~
 
-for r in pulp pulp_puppet pulp_rpm pulp_ostree nectar pulp_openstack; do
-    echo "Cloning $r repo..."
-    git clone https://github.com/pulp/$r
+for proj in $projects; do
+    echo "Cloning $proj repo..."
+    git clone https://github.com/pulp/$proj
     echo "Creating packages..."
-    pushd $r
-    tito build --rpm --test
+    pushd $proj
+    if [ $proj = "pulp" ]; then
+        #If the project is pulp build a repo for each PR
+        yum-builddep -y $(ls | grep .spec)
+        for pr in $pullReqs; do
+            git fetch origin pull/$pr/head:pr-$pr
+            git checkout pr-$pr
+            tito build --rpm --test
+            mkdir -p "$repoFolder$pr"
+            mv /tmp/tito/* "$repoFolder$pr"
+        done
+        git checkout master
+        tito build --rpm --test
+        mkdir -p "${repoFolder}master"
+        mv /tmp/tito/* "${repoFolder}master"
+    else
+        yum-builddep -y $(ls | grep .spec)
+        tito build --rpm --test
+        #Copy results for each folder
+        for pr in $pullReqs; do
+            cp -r /tmp/tito/* "$repoFolder$pr"
+            cp -r /tmp/tito/* "${repoFolder}master"
+        done
+        rm -fr /tmp/tito/*
+    fi
     popd
 done
 
-rm -fr /var/www/html/*
-cp -r /tmp/tito/* /var/www/html
-createrepo /var/www/html
+for pr in $pullReqs; do
+    createrepo "$repoFolder$pr"
+done
+createrepo "${repoFolder}master"
